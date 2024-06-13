61bae30fabbbc080e39370ba4cfadc29070819fe
naxsyambis <naxsyambis@gmail.com>
2024-06-13 10:51:29 +0100

    penginisialan variabel

--- BinarySearchTree_0163.cpp
+++ BinarySearchTree_0163.cpp
@@ -9,7 +9,13 @@ public:
     Node* leftchild;
     Node* rightchild;
     
-
+    // Constructor for the node class
+    Node(string i, Node* l, Node* r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
 
 };
 
