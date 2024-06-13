9a2657ffed4ed868c3af65d5b46641ad0af57369
naxsyambis <naxsyambis@gmail.com>
2024-06-13 11:16:48 +0100

    parent dan currentNode menjadi null search element, parent dan currentNode
    jika parent null
    jika data dalam new node kecil dari parent
    
    jika data dalam new node besar dari parent

--- BinarySearchTree_0163.cpp
+++ BinarySearchTree_0163.cpp
@@ -36,7 +36,29 @@ public:
         newNode->leftchild = nullptr; // make the left child of the new node point to null
         newNode->rightchild = nullptr; // make the right child of the newn node point to null
 
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); //locate the node which will be the parent of the node to be insert
 
+        if (parent == nullptr) // if the paren is null (tree is empty)
+        {
+            ROOT = newNode; // mark the new node as ROOT
+            return; // exit
+        }
+
+        if (element < parent->info) // if the value in the data field of the new node is less than that of the parent
+        {
+            parent->leftchild = newNode; // make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) // if the value in the data field of the new node is greater than that parent
+        {
+            parent->rightchild = newNode; // make the right child of the parent point to the new node
+        }
+
+    }
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
 
     }
 
