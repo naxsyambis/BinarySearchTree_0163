92d8304603764b88d5a2b207343c8ec451ebfb02
naxsyambis <naxsyambis@gmail.com>
2024-06-13 11:33:42 +0100

    void post order cout di akhir

--- BinarySearchTree_0163.cpp
+++ BinarySearchTree_0163.cpp
@@ -102,6 +102,21 @@ public:
         }
     }
 
+    void postorder(Node* ptr)
+    {
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            postorder(ptr->leftchild);  
+            postorder(ptr->rightchild); 
+            cout << ptr->info << " ";  
+        }
+    }
+
 
 
 };
