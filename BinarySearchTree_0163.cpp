818bbc72d78214577a827deeefab41337a1219d5
naxsyambis <naxsyambis@gmail.com>
2024-06-13 11:22:01 +0100

    current node = root parent = nullptr ketika currentnode tidak sama dengan nullptr dan currentnode data tidak sama dengan element

--- BinarySearchTree_0163.cpp
+++ BinarySearchTree_0163.cpp
@@ -59,7 +59,17 @@ public:
 
     void search(string element, Node*& parent, Node*& currentNode)
     {
-
+        // this function searches the currentNode of the specified node as well as the current node of its parent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
     }
 
 };
