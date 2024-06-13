c3e84545778c64b64c982be3605277e721bef19f
naxsyambis <naxsyambis@gmail.com>
2024-06-13 11:02:04 +0100

    memberikan memori untuk node memasukkan nilai kedalam new node membuat left child di new node point to null membuat right chid di newnode poin to null

--- BinarySearchTree_0163.cpp
+++ BinarySearchTree_0163.cpp
@@ -31,6 +31,12 @@ public:
 
     void insert(string element) // insert a node in the binary search tree
     {
+        Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for the node
+        newNode->info = element; // Assign value to the data field of the new node
+        newNode->leftchild = nullptr; // make the left child of the new node point to null
+        newNode->rightchild = nullptr; // make the right child of the newn node point to null
+
+
 
     }
 
